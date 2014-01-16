41c6ff2f8980b4573d4578ec07d05ae21bca08a3
anoopsinghbayes <anoopsingh.bayes@gmail.com>
2014-01-16 23:45:03 +0530

    google map working model

--- public/js/controllers/map.js
+++ public/js/controllers/map.js
@@ -1,34 +1,39 @@
-angular.module('mean').controller('MapCtrl', ['$scope', function ($scope) {
+angular.module('mean').controller('MapCtrl', ['$scope','$window','$timeout' ,function ($scope,$window,$timeout) {
     $scope.fromLocation='';
 	$scope.toLocation='';
+    $scope.distance=0;
 	$scope.mapOptions = {
       center: new google.maps.LatLng(18.9750, 72.8258),
       zoom: 7,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
+    var directionsDisplay;
+    $timeout(function(){
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    directionsDisplay.setMap($scope.myMap);
+},0);
+    $scope.findPath=function () {
+
+        $scope.directionsService = new google.maps.DirectionsService();
+
+        var request = {
+            origin: $scope.fromLocation,
+            destination: $scope.toLocation,
+            travelMode: google.maps.DirectionsTravelMode.DRIVING
+        };
+        $scope.directionsService.route(request, function(response, status) {
+
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+            $scope.distance= response.routes[0].legs[0].distance.value / 1000;
+
+        }
 
 
- var directionsDisplay = new google.maps.DirectionsRenderer($scope.myMap);
- var directionsService = new google.maps.DirectionsService();
-$scope.findPath=function () {
- 
- // $scope.$apply();
-  var request = {
-    origin:$scope.fromLocation,
-    destination:$scope.toLocation,
-    travelMode: google.maps.DirectionsTravelMode.DRIVING
-  };
-  
-	
-  directionsService.route(request, function(response, status) {
-    $scope.$apply(function(){
-	  if (status == google.maps.DirectionsStatus.OK) {
-      directionsDisplay.setDirections(response);
-    }
-	});
-  
-});
-}											 
+    });
+
+
+}
 
 }
 											 
--- public/js/directives.js
+++ public/js/directives.js
@@ -4,15 +4,16 @@
     return {
 		
 		restrict:'A',
-		link:function($scope, elm, attrs){
+        require: 'ngModel',
+		link:function($scope, elm, attrs,ctrl){
          var autocomplete = new google.maps.places.Autocomplete(elm[0], {});
 google.maps.event.addListener(autocomplete, 'place_changed', function() {
 $scope.$apply(function(){
 	var place = autocomplete.getPlace();
 //$scope.location = place.geometry.location.lat() + ',' + place.geometry.location.lng();
 	if(place.formatted_address)
-	elm.value=place.formatted_address;
-		console.log(elm.value);
+        ctrl.$setViewValue(place.formatted_address);
+
 });     
 });
       
