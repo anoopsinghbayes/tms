2b6717a175bd820e438b9aaa850dcd04d57e636a
unknown <eetuser@SYSTEM101.flexco.com>
2014-01-22 18:03:08 +0530

    beautified

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,25 +1,26 @@
 'use strict';
-        angular.module('mean').directive('places', function() {
- 
-    return {
-		
-		restrict:'A',
-        require: 'ngModel',
-		link:function($scope, elm, attrs,ctrl){
-         var autocomplete = new google.maps.places.Autocomplete(elm[0], {});
-google.maps.event.addListener(autocomplete, 'place_changed', function() {
-$scope.$apply(function(){
-	var place = autocomplete.getPlace();
-//$scope.location = place.geometry.location.lat() + ',' + place.geometry.location.lng();
-	if(place.formatted_address)
-        ctrl.$setViewValue(place.formatted_address);
+angular.module('mean').directive('places', function () {
 
-});     
-});
-      
- 
-   /* return {
+	return {
+
+		restrict: 'A',
+		require: 'ngModel',
+		link: function ($scope, elm, attrs, ctrl) {
+			var autocomplete = new google.maps.places.Autocomplete(elm[0], {});
+			google.maps.event.addListener(autocomplete, 'place_changed', function () {
+				$scope.$apply(function () {
+					var place = autocomplete.getPlace();
+					//$scope.location = place.geometry.location.lat() + ',' + place.geometry.location.lng();
+					if (place.formatted_address)
+						ctrl.$setViewValue(place.formatted_address);
+
+				});
+			});
+
+
+			/* return {
       link: link
     };*/
-  }}
-		 });
+		}
+	}
+});
