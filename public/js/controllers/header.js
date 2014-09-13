3ccdf6c85ae00159f0ee0bb0568c040acdd84a9a
anoopsinghbayes <anoopsingh.bayes@gmail.com>
2014-09-13 18:47:32 +0530

    reverted header.js

--- public/js/controllers/header.js
+++ public/js/controllers/header.js
@@ -2,87 +2,112 @@
 
 angular.module('mean.system').controller('HeaderController', ['$scope', 'Global','SlideMenu', function ($scope, Global,SlideMenu) {
     $scope.global = Global;
-     $scope.open=false;
-  $scope.openMenu=function(){
-    SlideMenu.toggleClass();
-    $scope.open=!$scope.open;
-    //console.log(SlideMenu);
-  }
+    $scope.open=false;
+    $scope.openMenu=function(){
+        SlideMenu.toggleClass();
+        $scope.open=!$scope.open;
+        //console.log(SlideMenu);
+    }
     $scope.menu = [
-        //, {
-        //'title': 'Edit Customer',
-        //'link': 'customers/edit',
-        //'state':'customers.edit'
-		
-    //}
 
-				  {
-        'title': 'Create BP',
-       // 'link': 'businessPartners/create',
-                      'state':'contacts'
-
-		
-    },			  {
-        'title': "Order",
-        'link': '',
-            'state':'Orders'
-
-		
-    },			 
-				   {
-        'title': "Map",
-        'link': 'map',
-            'state':'map'
-
-		
-    },			 
-				   {
-        'title': "Map2",
-        'link': 'map2',
-            'state':'map2'
-
-		
-    },
-    {
-        'title':"Customers",
-        'link':'Customers',
-        'state':'customers'
-    },
         {
-            'title':"Vehicles",
-            iconClass:"fa fa-automobile",
-            'link':'Vehicles',
-            'state':'addVehicle',
+            title: "Order",
+            iconClass:'fa fa-tasks',
+            link: '',
+            state:'',
+            isCollapsed:'true',
+            links:[
+                {
+                    title: 'Sales Order',
+                    state: 'salesOrder'
+                },
+                {
+                    title:'Purchase Order',
+                    state:''
+                },
+                {
+                    title:'Service Order',
+                    state:'serviceOrder'
+                }
+            ]
+
+
+
+        },
+        {
+            title:"Customers",
+            link:'Customers',
+            iconClass:'fa fa-users',
+            state:'customers'
+        },
+        {
+            title:"Vehicles",
+            iconClass:"fa fa-truck",
+            link:'Vehicles',
+            state:'',
             isCollapsed:true,
             links:[
                 {
-                    'title': "Vehicle  Details",
-                    'link': 'vehicleMaintainceDetails',
-                    'state':'vehicleMaintainceDetails'
+                    title:'List',
+                    state:'vehicles'
+                },
+                {
+                    title: "Maintenance",
+                    link: 'vehicleMaintainceDetails',
+                    state:'vehicleMaintainceDetails'
 
 
+                },{
+                    title:'Finance',
+                    state:'vehicles.finance'
                 }]
 
         },{
-           'title':'Tyre',
+            title:'Tyre',
+            iconClass:'fa fa-life-ring',
             link:'TyreDetails',
             state:'TyreDetails'
+        },{
+            'title':'Invoice',
+            link:'InvoiceDetails',
+            state:'InvoiceDetails'
+        },{
+            title:'Accounts',
+            iconClass:'fa fa-table',
+            link:'',
+            state:''
+        },{
+            title:'Employee',
+            iconClass:'fa fa-male',
+            link:'',
+            state:'employees'
+        },
+        {
+            title:'Reports',
+            iconClass:'fa fa-bar-chart-o',
+            link:'',
+            state:''
+        },{
+            title:'GPS Tracking',
+            iconClass:'fa fa-map-marker',
+            link:'',
+            state:''
         }
     ];
-    
+
     $scope.isCollapsed = false;
 }]);
 angular.module('mean').controller('TabsDemoCtrl',['$scope',function TabsDemoCtrl($scope) {
-  $scope.tabs = [
-    { title:"Dynamic Title 1", content:"Dynamic content 1" },
-    { title:"Dynamic Title 2", content:"Dynamic content 2", disabled: true }
-  ];
+    $scope.tabs = [
+        { title:"Dynamic Title 1", content:"Dynamic content 1" },
+        { title:"Dynamic Title 2", content:"Dynamic content 2", disabled: true }
+    ];
 
-  $scope.alertMe = function() {
-    setTimeout(function() {
-      alert("You've selected the alert tab!");
-    });
-  };
+    $scope.alertMe = function() {
+        setTimeout(function() {
+            alert("You've selected the alert tab!");
+        });
+    };
 
-  $scope.navType = 'pills';
+    $scope.navType = 'pills';
 }]) ;
