c127e64ac2bfa30fc2d1f97fb8d15eb128e04d31
swapnilj88 <swapnilj88@gmail.com>
2014-09-30 23:36:33 +0530

    swapnil
    Invoice Queries

--- app/controllers/GenerateInvoice.js
+++ app/controllers/GenerateInvoice.js
@@ -1,3 +1,33 @@
 /**
  * Created by swapnil on 20-Sep-14.
  */
+
+
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    Customer = mongoose.model('Customer'),
+    qs=require('qs'),
+    _ = require('lodash');
+
+exports.getInvDet = function(req, res) {
+    //check for search query string
+    //and parse the query string to json to use further
+    var searchQuery=qs.parse(req.query);
+    console.log(searchQuery);
+    Customer = mongoose.mtModel(req.user.tenant+'.Customer');
+    Customer.find(searchQuery)
+        .select('name _id contact address').exec(function(err, bps) {
+            if (err) {
+                res.render('error', {
+                    status: 500
+                });
+            } else {
+                res.jsonp(bps);
+            }
+        });
+};
+
--- app/controllers/OrdersByCustomer.js
+++ app/controllers/OrdersByCustomer.js
@@ -7,25 +7,42 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
+
     Order = mongoose.model('Order'),
     qs=require('qs'),
     _ = require('lodash');
 /**
  * Find Order by Customer
  */
-exports.show = function(req, res, next) {
-    console.log('tenantid:',req.user.tenant);
+exports.ordersByCustomer = function(req, res, next) {
     var searchCondition = qs.parse(req.query);
+    //var searchCondition = req.query;
+    console.log(searchCondition);
+    //console.log(id);
     Order = mongoose.mtModel(req.user.tenant+'.Order');
-    Order.find({customer:searchCondition}).select('').exec(function(err, order) {
-        console.log('error:',err);
-        console.log('customer:',customer);
+    Order.find(searchCondition).populate('_id customer status').exec(function(err, order) {
         if (err){
             return  res.render('error');
         }
         else{
             res.json(order);
         }
-
     });
+};
+
+exports.ordersDetails = function(req, res, next) {
+    var orderList = req.param('_id').split(',')
+
+    //validate and enlist only valid ids to be sent to the query
+    orderList= _.filter(orderList,function(id){return mongoose.Types.ObjectId.isValid(id)});
+    Order = mongoose.mtModel(req.user.tenant+'.Order');
+        Order.find({'_id': {$in:orderList}}).select('_id customer status').exec(function (err, order) {
+            if (err) {
+                console.log(err)
+                return  res.render('error');
+            }
+            else {
+                res.json(order);
+            }
+        });
 };
